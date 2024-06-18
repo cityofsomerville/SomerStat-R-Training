a56d9c391f0419890b39952312760e304abde1c3
Derek Schaadt <deschaadt@somervillema.gov>
2024-06-18 16:28:11 -0400

    another try

--- createPage.R
+++
@@ -1,9 +0,0 @@
-cache_dir <- "docs/_cache"
-
-if (dir.exists(cache_dir)) {
-  unlink(cache_dir, recursive = TRUE)
-}
-rmarkdown::run("docs/index.Rmd")
-
-shinylive::export(appdir = "myapp", destdir = "docs")
-httpuv::runStaticServer("docs")
