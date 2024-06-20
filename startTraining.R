cbce4322894cb3d013b6e262bf4f9a15e1974fc1
dschaadt32 <dschaadt@mit.edu>
2024-06-20 10:14:19 -0400

    Cleaning up the training content

--- startTraining.R
+++ startTraining.R
@@ -4,7 +4,6 @@ cache_dir <- "docs/_cache"
 if (dir.exists(cache_dir)) {
   unlink(cache_dir, recursive = TRUE)
 }
-rmarkdown::run("docs/index.Rmd")
 
 rsconnect::setAccountInfo(name='dschaadt',
                           token='975BBD964A5DAB59FB95D2995BA278D2',
@@ -12,3 +11,4 @@ rsconnect::setAccountInfo(name='dschaadt',
 
 rsconnect::deployDoc("docs/index.Rmd")
 
+rmarkdown::run("docs/index.Rmd")
