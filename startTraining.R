a1d150ab9e1f7be1a29825495a64e7c743dea560
dschaadt32 <dschaadt@mit.edu>
2024-06-20 16:48:10 -0400

    Training seems to be in an acceptable state

--- startTraining.R
+++ startTraining.R
@@ -5,10 +5,5 @@ if (dir.exists(cache_dir)) {
   unlink(cache_dir, recursive = TRUE)
 }
 
-rsconnect::setAccountInfo(name='dschaadt',
-                          token='975BBD964A5DAB59FB95D2995BA278D2',
-                          secret='/6L6A1r77riFPQotSrNP5V3oguFQa6QWT+/bOkon')
-
-rsconnect::deployDoc("docs/index.Rmd")
 
 rmarkdown::run("docs/index.Rmd")
---
+++ uploadToShinyApp.R
@@ -0,0 +1,6 @@
+
+rsconnect::setAccountInfo(name='dschaadt',
+                          token='975BBD964A5DAB59FB95D2995BA278D2',
+                          secret='/6L6A1r77riFPQotSrNP5V3oguFQa6QWT+/bOkon')
+
+rsconnect::deployDoc("docs/index.Rmd")
