7bd43254cadc462113e40b4576de04dcbaae3b61
dschaadt32 <dschaadt@mit.edu>
2025-11-29 09:56:47 -0500

    trying to fix the curl issue

--- uploadToShinyApp.R
+++ uploadToShinyApp.R
@@ -1,7 +1,15 @@
 
-options(rsconnect.check.certificate = FALSE)
-rsconnect::setAccountInfo(name='dschaadt',
-                          token = token,
-                          secret = secret)
+# roll back curl to a stable version for macos
+install.packages("http://cran.r-project.org/src/contrib/Archive/curl/curl_6.2.3.tar.gz",
+                 repos = NULL,
+                 type = "source")
 
+rsconnect::removeAccount('dschaadt', server = 'shinyapps.io') 
+
+# Re-run setAccountInfo with your credentials
+rsconnect::setAccountInfo(name= "dschaadt",
+                          token = token,
+                          secret = secret) 
+
+# Then try the deployment again
 rsconnect::deployDoc("docs/index.Rmd")
